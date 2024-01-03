a812e7cf6eb90ed2703505e7b888f811938d7025
Steve Korshakov <steve@korshakov.com>
2024-01-02 20:25:03 -0800

    fix: fix module init on web again

--- sources/browser.ts
+++ sources/browser.ts
@@ -1,14 +1,11 @@
 // Imports
-import * as ort from 'onnxruntime-common';
+import * as ort from 'onnxruntime-web';
 import { Environment } from './modules/Environment';
 
 // Exports
 export * from './common';
-export * from 'onnxruntime-common';
+export * from 'onnxruntime-web';
 export default ort;
 
 // Environment
 export const environment: Environment = 'web';
-
-// Init ONNX Runtime: this loads all required backends
-require('onnxruntime-web');
