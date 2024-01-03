86fc84960ba53b0eaf5c096a650aa6d95c955e8e
Steve Korshakov <steve@korshakov.com>
2024-01-02 20:05:35 -0800

    ref: trying to fix vite

--- sources/browser.ts
+++ sources/browser.ts
@@ -1,14 +1,11 @@
 // Imports
-import * as ort from 'onnxruntime-common';
+import * as ort from 'onnxruntime-web';
 import { Environment } from './modules/Environment';
 
 // Exports
 export * from './common';
-export * from 'onnxruntime-common';
+export * from 'onnxruntime-web'; // Can't just use require since some bundlers (e.g. Webpack) won't load module
 export default ort;
 
 // Environment
 export const environment: Environment = 'web';
-
-// Init ONNX Runtime: this loads all required backends
-require('onnxruntime-web');
